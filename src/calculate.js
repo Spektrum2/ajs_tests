7925f87ab90f897ab6fb5f4e53df7cc17a966148
admin <promitay@gmail.com>
2024-03-12 11:19:42 +0300

    Initial commit

---
+++ src/calculate.js
@@ -0,0 +1,10 @@
+export function calculateTotal(purchases, applyDiscount) {
+    const sum = purchases.reduce((acc, item) => acc + item.count * item.price, 0);
+    if(applyDiscount) {
+        return sum * 0.5;
+    }
+    // for (let i=0; i < purchases.length; i++) {
+    //     result += purchases[i].count * purchases[i].price;
+    // }
+    return sum;
+}
